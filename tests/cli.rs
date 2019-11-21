8dc52949e8e47da52912401d20bbff05eb1d6a1b
Evan Tatarka <evan.tatarka@willowtreeapps.com>
2019-11-21 15:33:35 -0500

    Format code

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,4 +1,3 @@
-
 use std::process::Command;
 
 use assert_cmd::cargo::CargoError;
@@ -8,12 +7,9 @@ use assert_cmd::prelude::*;
 fn hello_canterlot() -> Result<(), CargoError> {
     let mut cmd = Command::cargo_bin("fimpp")?;
 
-    cmd.arg("gallop")
-        .arg("examples/hello_canterlot.fpp");
+    cmd.arg("gallop").arg("examples/hello_canterlot.fpp");
 
-    cmd.assert()
-        .success()
-        .stdout("Hello, Canterlot!\n");
+    cmd.assert().success().stdout("Hello, Canterlot!\n");
 
     Ok(())
 }
@@ -22,16 +18,15 @@ fn hello_canterlot() -> Result<(), CargoError> {
 fn say_hello_to_everypony() -> Result<(), CargoError> {
     let mut cmd = Command::cargo_bin("fimpp")?;
 
-    cmd.arg("gallop")
-        .arg("examples/hello_everypony.fpp");
+    cmd.arg("gallop").arg("examples/hello_everypony.fpp");
 
-    cmd.assert()
-        .success()
-        .stdout("Hello, Rainbow Dash\n\
-        Hello, Pinkie Pie\n\
-        Hello, Fluttershy\n\
-        Hello, Rarity\n\
-        Hello, Applejack\n");
+    cmd.assert().success().stdout(
+        "Hello, Rainbow Dash\n\
+         Hello, Pinkie Pie\n\
+         Hello, Fluttershy\n\
+         Hello, Rarity\n\
+         Hello, Applejack\n",
+    );
 
     Ok(())
 }
@@ -41,13 +36,12 @@ fn say_hello_to_everypony() -> Result<(), CargoError> {
 fn math() -> Result<(), CargoError> {
     let mut cmd = Command::cargo_bin("fimpp")?;
 
-    cmd.arg("gallop")
-        .arg("examples/math.fpp");
+    cmd.arg("gallop").arg("examples/math.fpp");
 
-    cmd.assert()
-        .success()
-        .stdout("7\n\
-        10\n");
+    cmd.assert().success().stdout(
+        "7\n\
+         10\n",
+    );
 
     Ok(())
 }
@@ -56,22 +50,21 @@ fn math() -> Result<(), CargoError> {
 fn sends_hello_canterlot() -> Result<(), CargoError> {
     let mut cmd = Command::cargo_bin("fimpp")?;
 
-    cmd.arg("send")
-        .arg("examples/hello_canterlot.fpp");
+    cmd.arg("send").arg("examples/hello_canterlot.fpp");
 
     cmd.assert().success();
 
     let mut cc_cmd = Command::new("cc");
 
-    cc_cmd.arg("hello_canterlot.o")
-        .arg("-o").arg("hello_canterlot");
+    cc_cmd
+        .arg("hello_canterlot.o")
+        .arg("-o")
+        .arg("hello_canterlot");
 
     cc_cmd.assert().success();
 
     let mut report_cmd = Command::new("./hello_canterlot");
-    report_cmd.assert()
-        .success()
-        .stdout("Hello, Canterlot!\n");
+    report_cmd.assert().success().stdout("Hello, Canterlot!\n");
 
     Ok(())
 }
